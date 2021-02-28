6b7732ca91e153826348ab07988c113952df1d56
Yashkhandhadia <79782954+Yashkhandhadia@users.noreply.github.com>
2021-02-28 19:48:13 +0530

    Commit

---
+++ src/postLogin/FirstScreenPostLogin.java
@@ -0,0 +1,5 @@
+package postLogin;
+
+public class FirstScreenPostLogin {
+
+}
---
+++ src/postLogin/PostLoginPage.java
@@ -0,0 +1,50 @@
+package postLogin;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JMenuBar;
+import javax.swing.JButton;
+
+public class PostLoginPage extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					PostLoginPage frame = new PostLoginPage();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public PostLoginPage() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 780, 580);
+		
+		JMenuBar menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JButton depositBtn = new JButton("Deposit");
+		depositBtn.setBounds(70, 224, 89, 23);
+		contentPane.add(depositBtn);
+	}
+}
