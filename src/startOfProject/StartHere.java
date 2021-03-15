a894ff4347e71e5ae2e40b686ae20912bc3be4a6
Rishabh <rishabhsethiadsce@gmail.com>
2021-03-15 21:50:25 +0530

    Added Start Of Project

---
+++ src/startOfProject/StartHere.java
@@ -0,0 +1,12 @@
+package startOfProject;
+
+import preLoginScreen.zee;
+
+public class StartHere {
+
+	public static void main(String[] args) {
+		zee startOfProject = new zee();
+		startOfProject.setVisible(true);
+	}
+
+}
